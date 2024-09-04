acf0cf5f46fd5464fdfe2e7c3b9c187c92c18cec
Mohamed Riyas Ashraf Ali <riyassucces@gmail.com>
2024-09-04 07:37:33 +0530

    alter the axios connection

--- Client/src/Api/Axios.js
+++ Client/src/Api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:3000", // Replace with your server's base URL
+  baseURL: "https://to-do-app-aulw.onrender.com/", // Replace with your server's base URL
   withCredentials: true, // This will include cookies in every request
 });
 
